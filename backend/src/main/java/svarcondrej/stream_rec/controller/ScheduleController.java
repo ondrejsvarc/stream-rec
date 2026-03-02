43f7e81ff61c124b6b5939ffb5526813e5d41852
Ondřej Švarc <ondrej.svarc@czechitas.cz>
2026-03-02 12:54:32 +0100

    Basic frontend for creating jobs

--- backend/src/main/java/svarcondrej/stream_rec/controller/ScheduleController.java
+++ backend/src/main/java/svarcondrej/stream_rec/controller/ScheduleController.java
@@ -4,6 +4,9 @@ import org.springframework.http.ResponseEntity;
 import svarcondrej.stream_rec.dto.ScheduleRequestDto;
 import svarcondrej.stream_rec.model.RecordingSchedule;
 
+import java.util.List;
+
 public interface ScheduleController {
+    public ResponseEntity<List<RecordingSchedule>> getAllSchedules();
     public ResponseEntity<RecordingSchedule> createSchedule(ScheduleRequestDto request);
 }
