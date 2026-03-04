ece4707d659d69df67a77eea02b793f450b9cb59
Ondřej Švarc <ondrej.svarc@czechitas.cz>
2026-03-04 20:46:59 +0100

    Fixed sending of password hash to the user

--- backend/src/main/java/svarcondrej/stream_rec/model/User.java
+++ backend/src/main/java/svarcondrej/stream_rec/model/User.java
@@ -1,5 +1,6 @@
 package svarcondrej.stream_rec.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import svarcondrej.stream_rec.enums.Role;
@@ -17,6 +18,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String username;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 
