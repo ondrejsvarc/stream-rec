9a55613c2d5b40f125cd9dd5286628ac7e88e15b
Ondřej Švarc <ondrej.svarc@czechitas.cz>
2026-03-05 11:51:08 +0100

    Status without login

--- backend/src/main/java/svarcondrej/stream_rec/security/SecurityConfig.java
+++ backend/src/main/java/svarcondrej/stream_rec/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configure(http))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/auth/login", "api/status").permitAll()
                         .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
